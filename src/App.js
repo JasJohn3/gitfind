d879c96d4df2868e07ed9c39d2ec6eb48f0f0b29
Your Name <jasjohn3@uat.edu>
2021-07-27 08:34:42 -0700

    Initial State added to prop UserItem

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 import './App.css';
 export class App extends Component {
+
   render() {
     return (
       <div>
         <Navbar title="Github Finder" icon="fab fa-github"/>
+        <UserItem/>
       </div>
     )
   }
