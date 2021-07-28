0d9dc9e8423ed12d565af0394a298fa47c7d12c9
Your Name <jasjohn3@uat.edu>
2021-07-27 18:51:38 -0700

    UserItem update

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,37 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class UserItem extends Component {
-state ={
-      id:'id',
-      login:'mojombo',
-      avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-      gravatar_id: "",
-      url: "https://api.github.com/users/mojombo",
-      html_url: "https://github.com/mojombo",
-      followers_url: "https://api.github.com/users/mojombo/followers",
-      following_url: "https://api.github.com/users/mojombo/following{/other_user}",
-      gists_url: "https://api.github.com/users/mojombo/gists{/gist_id}",
-      starred_url: "https://api.github.com/users/mojombo/starred{/owner}{/repo}",
-      subscriptions_url: "https://api.github.com/users/mojombo/subscriptions",
-      organizations_url: "https://api.github.com/users/mojombo/orgs",
-      repos_url: "https://api.github.com/users/mojombo/repos",
-      events_url: "https://api.github.com/users/mojombo/events{/privacy}",
-      received_events_url: "https://api.github.com/users/mojombo/received_events",
-      type: "User",
-      site_admin: false
-    }
+  state = {
+    id: 'id',
+    login: 'mojombo',
+    avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
+    gravatar_id: '',
+    url: 'https://api.github.com/users/mojombo',
+    html_url: 'https://github.com/mojombo',
+    followers_url: 'https://api.github.com/users/mojombo/followers',
+    following_url:
+      'https://api.github.com/users/mojombo/following{/other_user}',
+    gists_url: 'https://api.github.com/users/mojombo/gists{/gist_id}',
+    starred_url: 'https://api.github.com/users/mojombo/starred{/owner}{/repo}',
+    subscriptions_url: 'https://api.github.com/users/mojombo/subscriptions',
+    organizations_url: 'https://api.github.com/users/mojombo/orgs',
+    repos_url: 'https://api.github.com/users/mojombo/repos',
+    events_url: 'https://api.github.com/users/mojombo/events{/privacy}',
+    received_events_url: 'https://api.github.com/users/mojombo/received_events',
+    type: 'User',
+    site_admin: false,
+  };
   render() {
-    const{login,avatar_url,html_url} =this.state;
+    const { login, avatar_url, html_url } = this.state;
     return (
-      <div className="card text-center">
-        <img src={avatar_url} alt="" className="round-img" style={{width:'60px'}} />
+      <div className='card text-center'>
+        <img
+          src={avatar_url}
+          alt=''
+          className='round-img'
+          style={{ width: '60px' }}
+        />
         <h3>{login}</h3>
         <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1" alt="">Read More</a>
+          <a href={html_url} className='btn btn-dark btn-sm my-1' alt=''>
+            Read More
+          </a>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default UserItem
+export default UserItem;
